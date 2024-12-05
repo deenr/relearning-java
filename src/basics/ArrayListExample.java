33858896b1bc79ee8c60b95896685d2c8a39d40e
Dean Reymen <deenrayman@outlook.com>
2024-12-05 18:26:00 +0100

    feat: array list

---
+++ src/basics/ArrayListExample.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author reymen
+ */
+public class ArrayListExample {
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<String>();
+
+        names.add("Dean");
+
+        System.out.println(names); //[Dean]
+
+        names.add("Peter");
+        names.add("John");
+
+        System.out.println(names); //[Dean, Peter, John]
+        
+        names.set(1, "Joe");
+
+        System.out.println(names); //[Dean, Joe, John]
+    }
+}
