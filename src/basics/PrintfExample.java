75c47c09d88b4dd5cff4403ed5f1dbd3facae32b
Dean Reymen <deenrayman@outlook.com>
2024-12-06 09:54:37 +0100

    feat: printf

---
+++ src/basics/PrintfExample.java
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author reymen
+ */
+public class PrintfExample {
+    public static void main(String[] args) {
+        System.out.printf("Put a decimal here %d, and a string here %s. That is cool, right?", 10, "Test");
+    }
+}
